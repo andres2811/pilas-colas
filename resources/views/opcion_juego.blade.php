d849d82eb0ae089816488858c2eb8a9459aeabfe
Unknown <alex_aact@hotmail.com>
2018-04-11 19:11:38 -0500

    diseño vista: opcion_juego.blade.php
    se añade imagen, fondo oscuro al body y se crea el navbar de color rojo

--- resources/views/opcion_juego.blade.php
+++ resources/views/opcion_juego.blade.php
@@ -4,15 +4,27 @@
 
 
 	<!--Menu de mi master-->
-	<ul class="nav justify-content-center">
+<body class="container bg-dark">
+    <nav class="navbar navbar-expand-sm bg-danger">
+    	<font color="black"><h3 align="center">ELIGE UNA OPCION</h3></font>
+    	<hr>
+		<ul class="nav justify-content-center">
 		  <li class="nav-item">
-		    <a type="button" class="btn btn-primary" href="{{ URL::to('juegoDosMaster')}}">torres de hanoy</a>
+		  	<a class="nav-link" href=juegoDosMaster><font color="white">torres de hanoy</font></a>
+		    <!--<a type="button" class="btn btn-primary" href="{{ URL::to('juegoDosMaster')}}">torres de hanoy</a>-->
 		  </li>
 
 		  <li class="nav-item">
-		    <a type="button" class="btn btn-primary"" href="{{ URL::to('juegoUnoMaster')}}">systema de citas</a>
+		  	<a class="nav-link" href="juegoUnoMaster"><font color="white">sistema de citas</font></a>
+		    <!--<a type="button" class="btn btn-primary"" href="{{ URL::to('juegoUnoMaster')}}">sistema de citas</a>-->
 		  </li>
 		  
 		</ul>
+	</nav>
+	<br>
+	<center>
+		<img src="../public/imagenes/elige.jpg" width="600" class="rounded-circle">
+	</center>
+</body>
 
 @stop
